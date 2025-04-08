fc143b01f099870632ff8b726a4b7cdfbb6e31d0
Philippe Dupuis <philippe@braver.health>
2025-04-07 22:07:09 -0400

    Remove internal from ClientDisconnectReason

--- lib/src/types/internal.dart
+++ lib/src/types/internal.dart
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import 'package:meta/meta.dart';
-
-@internal
 enum ClientDisconnectReason {
   user,
   peerConnectionClosed,
